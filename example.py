8d1cd8bca6266cf8e1997e4bf56bdac41a5ee144
ludndev <ludndev@gmail.com>
2024-03-23 17:31:15 +0100

    refact example.py to add a graceful quit

--- example.py
+++ example.py
@@ -1,24 +1,37 @@
 from proxy_checker import ProxyChecker
+import signal
 import sources
 
 
-source = sources.SslProxiesSource()
-print(f"Proxies: {len(source.get_proxies())} in {source.__class__.__name__}")
+def main():
+    source = sources.SslProxiesSource()
+    print(f"Proxies: {len(source.get_proxies())} in {source.__class__.__name__}")
 
-source = sources.ProxyScrapeSource()
-print(f"Proxies: {len(source.get_proxies())} in {source.__class__.__name__}")
+    source = sources.ProxyScrapeSource()
+    print(f"Proxies: {len(source.get_proxies())} in {source.__class__.__name__}")
 
-# source = sources.DidSoftSource(email="email@example.com", password="password")
-# print(f"Proxies: {len(source.get_proxies())} in {source.__class__.__name__}")
+    # source = sources.DidSoftSource(email="email@example.com", password="password")
+    # print(f"Proxies: {len(source.get_proxies())} in {source.__class__.__name__}")
+
+    proxy_checker = ProxyChecker([
+        sources.SslProxiesSource(),
+        sources.ProxyScrapeSource(),
+    ])
+
+    proxy_checker.do_the_thing()
+
+    proxies = proxy_checker.get_proxy_list()
+
+    print(f"Proxies: {len(proxies)} in many `sources`")
 
 
-proxy_checker = ProxyChecker([
-    sources.SslProxiesSource(),
-    sources.ProxyScrapeSource(),
-])
+def handler(signum, frame):
+    print(f"Received SIGINT signal. Houston, we are going back home ...")
+    exit(1)
 
-proxy_checker.do_the_thing()
 
-proxies = proxy_checker.get_proxy_list()
+signal.signal(signal.SIGINT, handler)
 
-print(f"Proxies: {len(proxies)} in many `sources`")
+
+if __name__ == "__main__":
+    main()
