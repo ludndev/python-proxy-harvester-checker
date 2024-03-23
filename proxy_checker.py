efd8be922a1d426a503571c7e3198b7a1589b3ed
ludndev <ludndev@gmail.com>
2024-03-23 14:42:51 +0100

    add create_proxy_stored_file_name_if_not_exist to proxy_checker.py

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 
 class ProxyChecker:
     def __init__(self, source=None, file_name="proxy_list.txt", validity="https://google.com/"):
@@ -5,3 +8,8 @@ class ProxyChecker:
         self.source = source
         self.file_name = file_name
         self.validity = validity
+        self.create_proxy_stored_file_name_if_not_exist()
+
+    def create_proxy_stored_file_name_if_not_exist(self):
+        if not os.path.exists(self.file_name):
+            Path(self.file_name).touch()
