67157ba6a444135e04b371fdf0a520614636b208
ludndev <ludndev@gmail.com>
2024-03-23 17:33:53 +0100

    refact example.py to add try clause

--- example.py
+++ example.py
@@ -34,4 +34,7 @@ signal.signal(signal.SIGINT, handler)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Houston, some nasty alien said something. {e}")
