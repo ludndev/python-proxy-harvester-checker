14a5f0eaede473b9c52483736900758fa090d4a0
ludndev <ludndev@gmail.com>
2024-03-23 14:48:03 +0100

    add build_proxy_dict to proxy_checker.py

--- proxy_checker.py
+++ proxy_checker.py
@@ -13,3 +13,25 @@ class ProxyChecker:
     def create_proxy_stored_file_name_if_not_exist(self):
         if not os.path.exists(self.file_name):
             Path(self.file_name).touch()
+
+    def build_proxy_dict(self, raw_proxy):
+        proxy_dict = {}
+
+        raw_dict = raw_proxy.split('|')
+
+        if raw_dict[0] != 'socks4' and raw_dict[0] != 'socks5':
+            proxy_dict['http'] = f'https://{raw_dict[2]}'
+            if raw_dict[1] == 'True':
+                proxy_dict['https'] = f'https://{raw_dict[2]}'
+
+        if raw_dict[0] == 'socks4':
+            proxy_dict['http'] = f'socks4://{raw_dict[2]}'
+            if raw_dict[1] == 'True':
+                proxy_dict['https'] = f'socks4://{raw_dict[2]}'
+
+        if raw_dict[0] == 'socks5':
+            proxy_dict['http'] = f'socks5://{raw_dict[2]}'
+            if raw_dict[1] == 'True':
+                proxy_dict['https'] = f'socks5://{raw_dict[2]}'
+
+        return proxy_dict
