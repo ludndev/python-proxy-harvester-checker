07c3d84f3465c6c44d4c8cfde4ac22a4211a5505
ludndev <ludndev@gmail.com>
2024-03-24 00:56:29 +0100

    add more case to tests for proxies' sources

--- test/test_proxy_sources.py
+++ test/test_proxy_sources.py
@@ -7,7 +7,18 @@ class TestProxySources(unittest.TestCase):
         self.proxies = {}
 
     def test_get_proxies_from_all_sources(self):
+        sources_classes = sources.get_sources(auth=None)
+        self.fetch_and_assert_proxies(sources_classes)
+
+    def test_get_proxies_with_auth_true(self):
+        sources_classes = sources.get_sources(auth=True)
+        self.fetch_and_assert_proxies(sources_classes)
+
+    def test_get_proxies_with_auth_false(self):
         sources_classes = sources.get_sources(auth=False)
+        self.fetch_and_assert_proxies(sources_classes)
+
+    def fetch_and_assert_proxies(self, sources_classes):
         for source_class in sources_classes:
             source_instance = source_class()
             if source_class.__name__ not in self.proxies:
