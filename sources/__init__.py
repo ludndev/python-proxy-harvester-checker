73f9fdbfabbae3b4c29514518558424ed7455ed9
ludndev <ludndev@gmail.com>
2024-03-24 00:00:00 +0100

    retrieve sources list

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,4 +1,23 @@
-from sources.interface_source import *
-from sources.proxyscrape import *
-from sources.sslproxies import *
-from sources.didsoft import *
+import sources
+from .interface_source import *
+from .proxyscrape import *
+from .sslproxies import *
+from .didsoft import *
+
+
+def get_sources(auth=None):
+    # Get all classes from sources module
+    source_classes = [cls for name, cls in sources.__dict__.items() if isinstance(cls, type)]
+    source_classes = [cls for cls in source_classes
+                      if issubclass(cls, sources.SourceInterface) and cls != sources.SourceInterface]
+    classes = []
+    for source_class in source_classes:
+        source_instance = source_class()
+        if isinstance(auth, bool) and source_instance.auth == auth:
+            classes.append(source_class)
+            continue
+        classes.append(source_class)
+    return classes
+
+
+__all__ = ['get_sources']
